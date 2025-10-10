449ca9920f8eac140317c04a70077c5036332580
Winner Edwin <winneredwin2667@gmail.com>
2025-10-10 18:09:59 +0100

    hot fixes

--- components/profile/profile-edit-form.tsx
+++ components/profile/profile-edit-form.tsx
@@ -414,6 +414,7 @@ export function ProfileEditForm({ init }: { init: ProfileFormValues }) {
                           Allow your profile information to be displayed live on
                           the website. You can change this setting at any time.
                         </FormDescription>
+                        <FormMessage />
                       </div>
                     </FormItem>
                   )}
--- loaders/user.ts
+++ loaders/user.ts
@@ -2,10 +2,16 @@ import { authorizeSession } from "@/actions";
 import type { ProfileFormValues } from "@/schemas";
 
 export async function getUsersDetails(): Promise<ProfileFormValues> {
-  const { user, extras } = await authorizeSession();
+  const {
+    user,
+    extras: { work_role, publishData, ...extras },
+  } = await authorizeSession();
   const names = user.name.split(" ");
+  // @ts-expect-error idk what's going on fr
   return {
     ...extras,
+    work_role: work_role ?? "#",
+    publishData: publishData ?? false,
     firstName: names.shift() ?? "",
     lastName: names.pop() ?? "",
     image: user.image,
